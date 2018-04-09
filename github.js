6a88511398352a4c128e873b0bd75b119a5a5171
Samuel Aguiar <samuelaguiarp@gmail.com>
2018-04-09 17:30:05 -0300

    add files

---
+++ github.js
@@ -0,0 +1,22 @@
+class GitHub {
+  constructor() {
+    this.client_id = '06fb28ed47106b03b7e6';
+    this.client_secret = 'abe245cfa14fb27ab4fe1a61a750331b38dbed07';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const reposResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await reposResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
